159822bf9bad2706e835cb6c08311ab973603359
Yves Huber <yves.huber@lernende.bbw.ch>
2022-06-30 10:56:29 +0200

    added create user function

--- src/main/java/ch/bbw/personenverwaltung/MainController.java
+++ src/main/java/ch/bbw/personenverwaltung/MainController.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -46,6 +48,20 @@ public class MainController {
         model.addAttribute("verwaltung", dataRepository.findById(id));
 
         return "personenverwaltung";
+    }
+
+    @GetMapping("/create")
+    public String getcreate(Model model){
+        model.addAttribute("person", new Person());
+        return "create";
+    }
+    @PostMapping("/create")
+    public String create(@ModelAttribute Person person, Model model){
+        model.addAttribute("person", new Person());
+        System.out.println(person.getName());
+
+
+        return "create";
     }
 
 }
